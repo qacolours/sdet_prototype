5cf5c3126126ee0bf129dfa41250caccd926632f
Koustav Das <qacolours@gmail.com>
2024-02-22 00:39:51 +0530

    initial commit

---
+++ sdet_prototype/src/sdet_prototype/test_basics.java
@@ -0,0 +1,12 @@
+package sdet_prototype;
+
+import org.testng.annotations.*;
+
+
+public class test_basics {
+
+	@Test
+	public void just_print() {
+		System.out.println("just check if this message is getting printed");
+	}
+}
